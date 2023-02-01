73cf0e804884dfeaa67c1d9782a5adfc832070c9
Michele Puetz <puetz@b-tu.de>
2023-02-01 11:52:23 +0100

    Made config files compatible with reusable structure

--- numerical-study/1.2_jacobi_eigensolve_benchmark/1.2.2_eigensolve_benchmark_moments/postprocess_config_all.py
+++ numerical-study/1.2_jacobi_eigensolve_benchmark/1.2.2_eigensolve_benchmark_moments/postprocess_config_all.py
@@ -7,8 +7,16 @@ Parameters for postprocessing script.
 data_file_pattern = "data_nmom"
 
 # Source directories and corresponding labels
-source_dirs = ["intel_np1"]
-labels = ["Intel"]
+source_dirs = [
+                "intel_np1",
+                "intel_np6",
+                "gnu_np6",
+            ]
+labels = [
+            "Intel (1 core)",
+            "Intel (6 cores)",
+            "GNU (1 core)",
+        ]
 
 # Main directory for complete analysis
 main_dir = "intel_np1"
@@ -22,32 +30,64 @@ infile_prefix = ""
 # Common suffix of all data files after the number of moments
 infile_suffix = ".out"
 
+# Plot histograms of errors
+plot_histograms = True
+
 # Dictionary that maps configuration names used in data files to labels used for plots
 config_to_label_map = { \
-        "EigenTridiagonalSymmEigenlibQR None": \
-            "QR algorithm (Eigen)", \
-        "EigenTridiagonalSymmEigenlibQR LinearVandermondeSolver":
-            "QR algorithm (Eigen) + Vandermonde solver", \
-        "EigenTridiagonalSymmLapackQR None": \
+        "EigenTridiagonalSymmEigenlibQR+None": \
+            "QR (Eigen)", \
+        "EigenTridiagonalSymmEigenlibQR+LinearVandermondeSolver":
+            "QR (Eigen) + Vandermonde solver", \
+        "EigenTridiagonalSymmLapackQR+None": \
             "QR (LAPACK)", \
-        "EigenTridiagonalSymmLapackQR LinearVandermondeSolver": \
-            "QR algorithm (LAPACK) + Vandermonde solver", \
-        "EigenTridiagonalSymmLapackRRR None": \
-            "RRR algorithm (LAPACK)", \
-        "EigenTridiagonalSymmLapackRRR LinearVandermondeSolver": \
-            "RRR algorithm (LAPACK) + Vandermonde solver"
+        "EigenTridiagonalSymmLapackQR+LinearVandermondeSolver": \
+            "QR (LAPACK) + Vandermonde solver", \
+        "EigenTridiagonalSymmLapackRRR+None": \
+            "RRR (LAPACK)", \
+        "EigenTridiagonalSymmLapackRRR+LinearVandermondeSolver": \
+            "RRR (LAPACK) + Vandermonde solver"
                      }
 
 # Dictionary that maps configuration names used in data files to labels used for plots
 _errors = [
-            "MomentsRelError2Norm", \
-            "MomentsRelErrorInfNorm", \
-            "QuadratureNodesRelError2Norm", \
-            "QuadratureWeightsRelError2Norm"
             ]
 error_to_label_map = { \
                         e: e for e in _errors
                       }
+
+# Dictionary that maps configuration names used in data files to labels used for plots
+error_to_label_map = \
+    {
+            "MomentsRelError2Norm": \
+                "Rel. moment error (euclidean norm)",
+            "MomentsRelErrorInfNorm": \
+                "Max. rel. moment error",
+            "ComputingTime": \
+                "CPU time",
+            "RelativeSeparationCoefficient": \
+                "Rel. separation coeff."
+    }
+
+# Quantities used to measure distance to moment space boundary
+# (as in input filenames)
+boundary_dist_quantities = ["regularity-radius"]
+  #["sigma-min", "regularity-radius", "hankel-determinant", "beta-coeffs", "mom2nm2-boundary-dist"]
+
+# Names / labels corresponding to `quantities`
+boundary_dist_quantity_names = [r"$\mathrm{r_{reg}(\mathbf{M}_{2n-2})}$"]
+"""
+  [r"$\mathrm{\sigma_{min}}$",
+   r"$\mathrm{r_{reg}}$",
+   r"det($\mathrm{\mathbf{M}_{2n-2}})$",
+   r"$\mathrm{\beta_{min}}$",
+   r"$\mathrm{d_{2n-2}}$"]
+"""
+
+# functions to apply to input data
+funcs = {quantity: lambda x, _: x for quantity in boundary_dist_quantities}
+#funcs["hankel-determinant"] = lambda x, nmom: x**(2/nmom)
+#funcs["beta-coeffs"] = lambda x, _: np.min(x[:,1:], axis=1)
 
 # Target directory for output of figures
 target_dir = "fig"
